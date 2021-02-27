eb6f1964eef1a9d87d7b23804a0e8809d432489e
felipe.araujo <felipe.araujo@sankhya.com.br>
2021-02-27 00:39:51 -0300

    Criando casos de testes

--- src/main/java/br/com/felipe/rest/tests/BellyTest.java
+++ src/main/java/br/com/felipe/rest/tests/BellyTest.java
@@ -14,6 +14,20 @@ import br.com.felipe.rest.core.BaseTest;
 public class BellyTest extends BaseTest{
 	
 	private String token;
+	
+	private Movimentacao getMovimentacaoValida() {
+		Movimentacao mov = new Movimentacao();
+		mov.setConta_id(423740);
+//		mov.setUsuario_id(usuario_id);
+		mov.setDescricao("Pagamento de agua");
+		mov.setEnvolvido("Felipe");
+		mov.setTipo("REC");
+		mov.setData_transacao("01/01/2020");
+		mov.setData_pagamento("01/01/2021");
+		mov.setValor(100f);
+		mov.setStatus(true);
+		return mov;
+	}
 
 	@Before
 	public void login() {
@@ -52,7 +66,6 @@ public class BellyTest extends BaseTest{
 		.then()
 			.statusCode(201)
 		;
-		
 	}
 	
 	@Test
@@ -64,11 +77,115 @@ public class BellyTest extends BaseTest{
 		.when()
 			.put("/contas/423740")
 		.then()
-			.log().all()
 			.statusCode(200)
 			.body("nome", is("Conta de Teste 2.1 alterada"))
 		;
+	}
+	
+	@Test
+	public void naoDeveContaComMesmoNome() {
+		given()
+			.header("Authorization", "JWT " + token)
+			.body("{\"nome\": \"Conta de Teste 2.1 alterada\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.log().all()
+			.statusCode(400)
+			.body("error", is("Já existe uma conta com esse nome!"))
+		;
+	}
+
+	@Test
+	public void deveInserirMovimentacaoComSucesso() {
+		Movimentacao mov = getMovimentacaoValida();
 		
+		given()
+			.header("Authorization", "JWT " + token)
+			.body(mov)
+		.when()
+			.post("/transacoes")
+		.then()
+			.statusCode(201)
+		;
+	}
+
+	@Test
+	public void deveValidarCamposObrigatoriosMovimentacao() {
+		given()
+			.header("Authorization", "JWT " + token)
+			.body("{}")
+		.when()
+			.post("/transacoes")
+		.then()
+			.statusCode(400)
+			.body("$", hasSize(8))
+			.body("msg", hasItems(  
+					"Data da Movimentação é obrigatório",
+					"Data da Movimentação é obrigatório",
+					"Data do pagamento é obrigatório",
+					"Descrição é obrigatório",
+					"Interessado é obrigatório",
+					"Valor é obrigatório",
+					"Valor deve ser um número",
+					"Conta é obrigatório",
+					"Situação é obrigatório"))
+		;
+	}
+	
+	@Test
+	public void naoDeveInserirMovimentacaoComDataFutura() {
+		Movimentacao mov = getMovimentacaoValida();
+		mov.setData_transacao("28/02/2021");
+		
+		given()
+			.header("Authorization", "JWT " + token)
+			.body(mov)
+		.when()
+			.post("/transacoes")
+		.then()
+			.statusCode(400)
+			.body("$", hasSize(1))
+			.body("msg", hasItem("Data da Movimentação deve ser menor ou igual à data atual"))
+		;
+	}
+	
+	@Test
+	public void naoDeveRemoverContaComMovimentacao() {
+		given()
+			.header("Authorization", "JWT " + token)
+		.when()
+			.delete("/contas/427782")
+		.then()
+			.statusCode(500)
+			.body("constraint", is("transacoes_conta_id_foreign"))
+		;
+	}
+	
+	@Test
+	public void deveCalcularSaldoContas() {
+		given()
+			.header("Authorization", "JWT " + token)
+		.when()
+			.get("/saldo")
+		.then()
+			.statusCode(200)
+			.body("find{it.conta_id == 423740}.saldo", is("400.00"))
+//			.body("conta_id", hasItems("423740")) nao funcionou
+//			.body("conta", hasItem("Conta de Teste 2.1 alterada")) funcionou
+//			.body("saldo", hasItem("400.00")) funcionou
+		;
+	}
+	
+	@Test
+	public void deveRemoverMovimentacao() {
+		given()
+			.header("Authorization", "JWT " + token)
+		.when()
+			.delete("/transacoes/391832")
+		.then()
+			.statusCode(204)
+		;
 	}
 }
 
